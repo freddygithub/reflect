5a39d99260ef52bf9d4eb2e8fd61233138fda043
Frederick <fedmaster217@yahoo.com>
2019-05-01 13:22:07 -0400

    adding stripe

---
+++ js/stripe.js
@@ -0,0 +1,92 @@
+(function() {
+  'use strict';
+  
+  var elements = stripe.elements({
+    fonts: [
+      {
+        cssSrc: 'https://fonts.googleapis.com/css?family=Roboto',
+      },
+    ],
+    // Stripe's examples are localized to specific languages, but if
+    // you wish to have Elements automatically detect your user's locale,
+    // use `locale: 'auto'` instead.
+    locale: window.__exampleLocale
+  });
+
+  var card = elements.create('card', {
+    iconStyle: 'solid',
+    style: {
+      base: {
+        iconColor: '#c4f0ff',
+        color: '#fff',
+        fontWeight: 500,
+        fontFamily: 'Roboto, Open Sans, Segoe UI, sans-serif',
+        fontSize: '16px',
+        fontSmoothing: 'antialiased',
+
+        ':-webkit-autofill': {
+          color: '#fce883',
+        },
+        '::placeholder': {
+          color: 'white',
+        },
+      },
+      invalid: {
+        iconColor: '#FFC7EE',
+        color: '#FFC7EE',
+      },
+    },
+  });
+  card.mount('#example1-card');
+    
+    card.addEventListener('change', function(event) {
+        var displayError = document.getElementById('card-errors');
+            if (event.error) {
+                displayError.textContent = event.error.message;
+            } else {
+                displayError.textContent = '';
+            }
+        });
+    
+    var form = document.getElementById('payment-form');
+    
+    form.addEventListener('submit', function(event) {
+        
+        var plan = location.search;
+        plan = plan.charAt(6);
+        
+        //0 is yearly, 1 is monthly
+        if (plan != 1 && plan != 0) {
+            event.preventDefault();
+            alert("No plan selected, redirecting to Subscription page...");
+            window.location.href = "Plans.html";
+        } else {
+            event.preventDefault();
+            stripe.createToken(card).then(function(result) {
+                if (result.error) {
+                // Inform the user if there was an error.
+                var errorElement = document.getElementById('card-errors');
+                errorElement.textContent = result.error.message;
+                } else {
+                // Send the token to your server.
+                stripeTokenHandler(result.token);
+                }
+            });
+        }
+    });
+    
+    function stripeTokenHandler(token) {
+        // Insert the token ID into the form so it gets submitted to the server
+        var form = document.getElementById('payment-form');
+        var hiddenInput = document.createElement('input');
+        hiddenInput.setAttribute('type', 'hidden');
+        hiddenInput.setAttribute('name', 'stripeToken');
+        hiddenInput.setAttribute('value', token.id);
+        form.appendChild(hiddenInput);
+        // Submit the form
+        form.submit();
+}
+
+  registerElements([card], 'example1');
+})();
+
